c0dd2005fdef358c0cf716c2c518063b05f0c285
BoboLin <bobolincowo@gmail.com>
2021-06-11 14:43:51 +0800

    feat: add routes and views

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const express = require('express')
 const PORT = 3000
 const exphbs = require('express-handlebars')
 
+// routes setting
+const routes = require('./routes')
+
 // mongoose setting
 require('./config/mongoose')
 
 const app = express()
 
+// view - handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// use routes
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
