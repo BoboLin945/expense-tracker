5f5ff8c2411a0757ad009419cd4940673d4edcd9
BoboLin <bobolincowo@gmail.com>
2021-06-14 14:49:28 +0800

    feat: add Filter function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,16 +1,38 @@
 const express = require('express')
+const category = require('../../models/category')
 const record = require('../../models/record')
 const router = express.Router()
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
   Record.find()
     .lean()
     .then(records => {
-      // const date = records.forEach(record => { convertDate(record.date) })
-      res.render('index', { records })
+      Category
+      .find()
+      .lean()
+      .then(categories => {
+        res.render('index', { records, categories })
+      })
+      .catch(error => console.log(error))
     })
     .catch(error => console.log(error))
+})
+
+// filter by category
+router.post('/filter', (req, res) => {
+  const category = req.body.category
+  Record
+    .aggregate([{ $match: { category: category } }])
+    .then(records => {
+      Category
+        .find()
+        .lean()
+        .then(categories => {
+          res.render('index', { records, categories, category })
+        })
+    })
 })
 
 
